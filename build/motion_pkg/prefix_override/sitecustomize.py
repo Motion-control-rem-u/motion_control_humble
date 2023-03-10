9d84f8a838171848c7745f3c46212f7c1e2254b4
sebastian guayacan <s.guayacan@uniandes.edu.co>
2023-03-10 16:50:57 -0500

    prueba

--- build/motion_pkg/prefix_override/sitecustomize.py
+++ build/motion_pkg/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jcb/robocol/motion_control_ros2/install/motion_pkg'
+sys.prefix = sys.exec_prefix = '/home/sebastian/robocol/motion_control_humble/install/motion_pkg'
